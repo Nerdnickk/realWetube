37b793b5810983234813fcd743d4a8eb0dba9fe8
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-04-16 18:53:20 +0900

    realWetube

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:323232,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Contentbgi_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Kim",
+            email: "kcdmlry@naver.com"
+        }
+    },
+    {
+        id:1243231,
+        title: 'Video super',
+        description: 'This is something I want',
+        views: 12,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Contentbgi_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 323232,
+            name: "Kim!!!",
+            email: "kcdmlry@gmail.com"
+        }
+    },
+    {
+        id:123123,
+        title: 'Video love',
+        description: 'This is something I fuck',
+        views: 12,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Contentbgi_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 323232,
+            name: "Kim!!!",
+            email: "kcdmlry@gmail.com"
+        }
+    },
+    {
+        id:4657453,
+        title: 'Video eating',
+        description: 'This is something I eating',
+        views: 12,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Contentbgi_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 323232,
+            name: "Kim!!!",
+            email: "kcdmlry@gmail.com"
+        }
+    }
+]
---
+++ routers/globalRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+import routes from "../routes";
+import { home, search } from "../controller/videoController";
+import { getJoin, getLogin, logout, postJoin, postLogin } from "../controller/userController";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+globalRouter.get(routes.logout, logout);
+
+export default globalRouter;
